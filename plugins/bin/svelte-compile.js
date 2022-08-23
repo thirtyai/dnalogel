148da20725b8ba7b542dc721e36d12da2938cb9b
kyle ju <66406654+thirtyai@users.noreply.github.com>
2022-08-23 14:18:57 +0800

    feat: 支持scss; (#83)
    * feat: 支持scss; 代码梳理
    
    * fix: postcss集中配置,并修改root value and min pixel value
    
    * setting: 改回rootValue,minPixelValue
    
    Co-authored-by: kyle ju <liguo.ju@game000.com>

--- plugins/bin/svelte-compile.js
+++ plugins/bin/svelte-compile.js
@@ -1,29 +1,37 @@
 #!/usr/bin/env node
-
+/* eslint-disable @typescript-eslint/no-require-imports */
 const glob = require('glob')
 const path = require('path')
 const replace = require('replace')
-const { compile } = require('svelte/compiler')
-const  { readFile, writeFile, unlink } = require('fs/promises')
-
+const sveltePreprocessor = require('svelte-preprocess')
+const { compile, preprocess } = require('svelte/compiler')
+const { readFile, writeFile, unlink } = require('fs/promises')
+const postcssConfig = require('../postcss.config')
 const libsPath = path.resolve(path.resolve(), 'libs')
 
 replace({
-	regex: /\.svelte/g,
-	replacement: "",
-	paths: [libsPath],
-	recursive: true,
+  regex: /\.svelte/g,
+  replacement: '',
+  paths: [libsPath],
+  recursive: true,
+})
+const processes = sveltePreprocessor({
+  scss: {},
+  postcss: {
+    plugins: postcssConfig.plugins,
+  },
 })
 
 const sveltes = glob.sync('**/*.svelte', { cwd: libsPath })
 
 const run = async () => {
-	for (const svelte1 of sveltes.map((svelte) => path.resolve(libsPath, svelte))) {
-		const code = ( await readFile(svelte1) ).toString()
-		const { js } = compile(code, { css: true })
-		await writeFile(svelte1.replace('.svelte', '.js'), js.code)
-		await unlink(path.resolve(libsPath, svelte1))
-	}
+  for (const svelte1 of sveltes.map((svelte) => path.resolve(libsPath, svelte))) {
+    let code = (await readFile(svelte1)).toString()
+    code = await preprocess(code, processes)
+    const { js } = compile(code.code, { css: true })
+    await writeFile(svelte1.replace('.svelte', '.js'), js.code)
+    await unlink(path.resolve(libsPath, svelte1))
+  }
 }
 
-run().then(r => console.log("✨ svelte compile done！ ✨"))
+run().then((r) => console.log('✨ svelte compile done！ ✨'))
--- plugins/rollup.config.js
+++ plugins/rollup.config.js
@@ -2,31 +2,22 @@ import glob from 'glob'
 import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 import svelte from 'rollup-plugin-svelte'
-import sveltePreprocessor from "svelte-preprocess"
-import commonjs from "@rollup/plugin-commonjs"
+import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
+import sveltePreprocessor from 'svelte-preprocess'
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const postcssConfig = require('./postcss.config')
 const plugins = glob.sync('**/**Plugin/index.ts')
 
 const svelteRollupPlugin = svelte({
   emitCss: false,
-  extensions: [".svelte"],
+  extensions: ['.svelte'],
   preprocess: sveltePreprocessor({
-    postcss: {plugins: [
-      require('autoprefixer'),
-      require('postcss-plugin-px2rem')({
-        rootValue: 19.2, // 换算基数，1rem相当于10px,值为37.5时,1rem为20px,淘宝的flex默认为1rem为10px
-        // unitPrecision: 5, //允许REM单位增长到的十进制数字。
-        // propWhiteList: [],  //默认值是一个空数组，这意味着禁用白名单并启用所有属性。
-        // propBlackList: ['font-size', 'border'], // 黑名单
-        exclude: /(node_module)/, // 默认false，可以（reg）利用正则表达式排除某些文件夹的方法，例如/(node_module)\/如果想把前端UI框架内的px也转换成rem，请把此属性设为默认值
-      // selectorBlackList: [], //要忽略并保留为px的选择器
-      // ignoreIdentifier: false,  //（boolean/string）忽略单个属性的方法，启用 ignoreIdentifier 后，replace 将自动设置为 true。
-      // replace: true, // （布尔值）替换包含REM的规则，而不是添加回退。
-      mediaQuery: false, // （布尔值）允许在媒体查询中转换px。
-      minPixelValue: 3, // 设置要替换的最小像素值(3px会被转rem)。 默认 0
-      })
-    ]}
+    scss: {},
+    postcss: {
+      plugins: postcssConfig.plugins,
+    },
   }),
 })
 
@@ -44,69 +35,60 @@ const external = [
   // 'svelte/transition',
 
   // '@tweenjs/tween.js',
-
 ]
 const config = plugins.map((plugin) => {
   const pluginName = plugin.match(/\/((\w+)Plugin)\/index.ts/)[1]
   const pluginPath = plugin.replace('src/', 'dist/').replace('/index.ts', '/index.js')
   return defineConfig({
     input: plugin,
-    output: [{
-      name: pluginName,
-      file: pluginPath,
-      format: 'cjs'
-    }],
+    output: [
+      {
+        name: pluginName,
+        file: pluginPath,
+        format: 'cjs',
+      },
+    ],
     plugins: [
       svelteRollupPlugin,
       typescript({}),
       commonjs(),
-      postcss({plugins: [
-          require('autoprefixer'),
-          require('postcss-plugin-px2rem')({
-            rootValue: 19.2, // 换算基数，1rem相当于10px,值为37.5时,1rem为20px,淘宝的flex默认为1rem为10px
-            // unitPrecision: 5, //允许REM单位增长到的十进制数字。
-            // propWhiteList: [],  //默认值是一个空数组，这意味着禁用白名单并启用所有属性。
-            // propBlackList: ['font-size', 'border'], // 黑名单
-            exclude: /(node_module)/, // 默认false，可以（reg）利用正则表达式排除某些文件夹的方法，例如/(node_module)\/如果想把前端UI框架内的px也转换成rem，请把此属性设为默认值
-            // selectorBlackList: [], //要忽略并保留为px的选择器
-            // ignoreIdentifier: false,  //（boolean/string）忽略单个属性的方法，启用 ignoreIdentifier 后，replace 将自动设置为 true。
-            // replace: true, // （布尔值）替换包含REM的规则，而不是添加回退。
-            mediaQuery: false, // （布尔值）允许在媒体查询中转换px。
-            minPixelValue: 3, // 设置要替换的最小像素值(3px会被转rem)。 默认 0
-          })
-        ]})
+      postcss({
+        plugins: postcssConfig.plugins,
+      }),
     ],
     external,
   })
 })
 
-config.push(defineConfig({
-  input: 'src/index.ts',
-  output: [{
-    name: 'dnalogel',
-    file: `dist/index.js`,
-    format: 'cjs'
-  }, {
-    name: 'dnalogel',
-    file: `dist/index.es.js`,
-    format: 'es'
-  }, {
-    name: 'dnalogel',
-    file: `dist/index.umd.js`,
-    format: 'umd',
-    globals: {
-      '@realsee/five': 'five',
-      '@realsee/five/line': 'FiveSDK_Line',
-      three: 'THREE',
-      'three/examples/jsm/renderers/CSS3DRenderer': 'CSS3DRenderer'
-    },
-  }],
-  plugins: [
-    svelteRollupPlugin,
-    typescript({ tsconfig: './tsconfig.build.json' }),
-    commonjs(),
-  ],
-  external,
-}))
+config.push(
+  defineConfig({
+    input: 'src/index.ts',
+    output: [
+      {
+        name: 'dnalogel',
+        file: `dist/index.js`,
+        format: 'cjs',
+      },
+      {
+        name: 'dnalogel',
+        file: `dist/index.es.js`,
+        format: 'es',
+      },
+      {
+        name: 'dnalogel',
+        file: `dist/index.umd.js`,
+        format: 'umd',
+        globals: {
+          '@realsee/five': 'five',
+          '@realsee/five/line': 'FiveSDK_Line',
+          three: 'THREE',
+          'three/examples/jsm/renderers/CSS3DRenderer': 'CSS3DRenderer',
+        },
+      },
+    ],
+    plugins: [svelteRollupPlugin, typescript({ tsconfig: './tsconfig.build.json' }), commonjs()],
+    external,
+  }),
+)
 
 export default config
